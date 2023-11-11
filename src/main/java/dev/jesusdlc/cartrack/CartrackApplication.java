d74e04495cc30b396f3374544a5177a64e9c155b
jesus <jesusdlc1830@gmail.com>
2023-11-10 20:50:47 -0600

    desarrollo de estructura del proyecto

---
+++ src/main/java/dev/jesusdlc/cartrack/CartrackApplication.java
@@ -0,0 +1,13 @@
+package dev.jesusdlc.cartrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jesusdlc/cartrack/CartrackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jesusdlc.cartrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
